f96ee308b46a76aaac4550746f9efdd6e58c572e
Luis <rivasl.works@gmail.com>
2018-04-02 17:37:12 -0300

    Add suppor for command php route:list

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -82,6 +82,9 @@ $app->singleton(
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+// Add support for command php artisan route:list in Lumen
+$app->register(Appzcoder\LumenRoutesList\RoutesCommandServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
